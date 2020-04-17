c14c63c712c46d79b9db4ca2dff2419a6da58edb
DavidAbram <david@abram.com>
2020-04-17 16:28:47 +0200

    added new examples

---
+++ exercise-2/change-image/script.js
@@ -0,0 +1,25 @@
+function addNewImage() {
+  const imageElement = document.createElement('img');
+  const rand = Math.floor((Math.random() * 400) + 100);
+  imageElement.src = `https://source.unsplash.com/random/${rand}x${rand}`;
+  imageElement.onmouseenter = changeSize;
+  document.body.appendChild(imageElement);
+}
+
+function changeSize(event) {
+  const rand = Math.floor((Math.random() * 400) + 100);
+  event.target.src = `https://source.unsplash.com/random/${rand}x${rand}`;
+}
+
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
+addNewImage();
---
+++ exercise-2/table/script.js
@@ -0,0 +1,44 @@
+function deletePerson(event) {
+  const tableElement = document.querySelector('#table > tbody');
+  tableElement.removeChild(event.target.parentElement.parentElement)
+}
+
+function createNewPerson() {
+
+  const nameElement = document.querySelector('#name');
+  const surnameElement = document.querySelector('#surname');
+  const ageElement = document.querySelector('#age');
+
+  const trElement = document.createElement('tr');
+  const nameTdElement = document.createElement('td');
+  const surnameTdElement = document.createElement('td');
+  const ageTdElement = document.createElement('td');
+  const removeTdElement = document.createElement('td');
+
+  nameTdElement.textContent = nameElement.value;
+  surnameTdElement.textContent = surnameElement.value;
+  ageTdElement.textContent = ageElement.value;
+
+  const removeButtonElement = document.createElement('input');
+  removeButtonElement.type = 'button';
+  removeButtonElement.value = 'remove';
+  removeButtonElement.onclick = deletePerson;
+
+  removeTdElement.appendChild(removeButtonElement);
+
+
+  trElement.appendChild(nameTdElement);
+  trElement.appendChild(surnameTdElement);
+  trElement.appendChild(ageTdElement);
+  trElement.appendChild(removeTdElement);
+
+  const tableElement = document.querySelector('#table > tbody');
+  const createNewPersonElement = document.querySelector('#create-new-person');
+
+  tableElement.insertBefore(trElement, createNewPersonElement);
+
+}
+
+
+const commitElement = document.querySelector('#commit');
+commitElement.onclick = createNewPerson;
