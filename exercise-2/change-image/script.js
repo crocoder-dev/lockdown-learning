be1a646b187f3600f060a1da9820230e164fd8cc
DavidAbram <david@abram.com>
2020-04-18 10:50:29 +0200

    update

--- exercise-2/change-image/script.js
+++ exercise-2/change-image/script.js
@@ -1,3 +1,3 @@
 // Write a function that creates img elements that have a random image from source.unsplash (example: https://source.unsplash.com/random/300x300)
 
-// Make it so that every time mouse enter the image it reloads in some other image
+// Make it so that every time mouse enters the image it reloads some other image
