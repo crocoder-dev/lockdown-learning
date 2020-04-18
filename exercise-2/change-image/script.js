06a5e2edae72d06a60f747ee9775a19d224bb77c
DavidAbram <david@abram.com>
2020-04-18 08:50:13 +0200

    added some comments in scripts

---
+++ exercise-2/change-image/script.js
@@ -0,0 +1,3 @@
+// Write a function that creates img elements that have a random image from source.unsplash (example: https://source.unsplash.com/random/300x300)
+
+// Make it so that every time mouse enter the image it reloads in some other image
---
+++ exercise-2/table/script.js
@@ -0,0 +1,12 @@
+// create function that gets data from the inputs and create a new person table row 
+
+/* 
+      <tr>
+        <td>David</td>
+        <td>Abram</td>
+        <td>30</td>
+        <td><input type="button" value="remove" /></td>
+      </tr>
+*/
+
+// set the onclick of the button so it removes the table row from the table
