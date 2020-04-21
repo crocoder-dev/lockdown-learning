ee38e0ace72c0cf540c6bb6e9fe3dc98cbb8c013
DavidAbram <david@abram.com>
2020-04-21 12:38:33 +0200

    added for-map-reduce

---
+++ for-map-reduce/script.js
@@ -0,0 +1,43 @@
+const pizzas = [
+  {
+    name: 'Calzone',
+    toppings: ['Tomato sauce', 'Cheese', 'Ham', 'Oregano'],
+  },
+  {
+    name: 'Hawaiian',
+    toppings: ['Tomato sauce', 'Cheese', 'Pineapple'],
+  },
+  {
+    name: 'Frutti di mare',
+    toppings: ['Tomato sauce', 'shellfish', 'squid', 'clams', 'scampi'],
+  },
+  {
+    name: 'Margherita',
+    toppings: ['Tomato sauce', 'Mozzarella', 'Basil'],
+  },
+];
+
+const comments = [
+  {
+    text: 'I love pizza',
+    rating: 5,
+  },
+  {
+    text: 'Why pineapple?',
+    rating: 2,
+  },
+  {
+    text: 'I am very dissapointed with your lack of sushi pizza.',
+    rating: 3,
+  },
+  {
+    text: 'Hey Alexa, how many pizzas can we fit in a Tesla?',
+    rating: 5,
+  },
+  {
+    text: 'Pizza was cold.',
+    rating: 1,
+  },
+];
+
+console.log(comments);
