0b96a827dde32725ac356b3545c7041765ba354a
DavidAbram <david@abram.com>
2020-04-25 09:51:27 +0200

    added type colors

--- pokemon-project/data.js
+++ pokemon-project/data.js
@@ -1,3 +1,24 @@
+const colors = {
+  Grass: '#78C850',
+  Poison: '#A040A0',
+  Fire: '#F08030',
+  Flying: '#A890F0',
+  Water: '#6890F0',
+  Bug: '#A8B820',
+  Normal: '#A8A878',
+  Electric: '#F8D030',
+  Ground: '#E0C068',
+  Fairy: '#EE99AC',
+  Fighting: '#C03028',
+  Psychic: '#F85888',
+  Rock: '#B8A038',
+  Ice: '#E0C068',
+  Dragon: '#7038F8',
+  Dark: '#705848',
+  Steel: '#B8B8D0',
+  Ghost: '#705898',
+};
+
 const pokemon = [{
   "id": 1,
   "name": {
